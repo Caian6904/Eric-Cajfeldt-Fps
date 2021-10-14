3ec1f71e2165fcd5bfa15e7677d465fdc732d484
Caian6904 <73233343+Caian6904@users.noreply.github.com>
2021-10-14 15:19:01 +0200

    Boom

--- Assets/GunScript.cs
+++ Assets/GunScript.cs
@@ -5,15 +5,20 @@ public class GunScript : MonoBehaviour
 {
     public float damage = 10f;
     public float range = 100f;
+    public float fireRate = 15f;
+    public float impactForce = 30f;
 
     public Camera fpsCam;
     public ParticleSystem MuzzleFlash;
 
+    private float nextTimeToFire = 0f;
+
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
         {
+            nextTimeToFire = Time.time + 1f / fireRate;
             Shoot();
         }
     }
@@ -31,6 +36,11 @@ public class GunScript : MonoBehaviour
             if (target != null)
             {
                 target.TakeDamage(damage);
+            }
+
+            if (hit.rigidbody != null)
+            {
+                hit.rigidbody.AddForce(-hit.normal * impactForce);
             }
         }
     }
